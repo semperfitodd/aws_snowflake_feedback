7c2bb529d5834db9a72f77c3cc5bbba12bcdceb5
Todd Bernson <todd@bernsonfamily.com>
2024-02-06 20:50:17 -0500

    creating consistency for colors on pie chart

--- terraform/static-site/src/utils.js
+++ terraform/static-site/src/utils.js
@@ -1,11 +1,22 @@
+const feedbackColors = {
+    MIXED: '#FFCE56',
+    NEGATIVE: '#FF6384',
+    NEUTRAL: '#36A2EB',
+    POSITIVE: '#4BC0C0',
+    UNKNOWN: '#C9CBCF'
+};
+
 export const processData = (data, period) => {
     const filteredData = data.filter(item => item.PERIOD === period);
+
+    const backgroundColors = filteredData.map(item => feedbackColors[item.FEEDBACK || 'UNKNOWN']);
+
     return {
         labels: filteredData.map(item => item.FEEDBACK || 'Unknown'),
         datasets: [{
             data: filteredData.map(item => item.RECCOUNT),
-            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0'],
-            hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0']
+            backgroundColor: backgroundColors,
+            hoverBackgroundColor: backgroundColors
         }]
     };
 };
